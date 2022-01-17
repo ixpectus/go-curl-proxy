0e91f3b310d7caa580ea1089ea57463344baf9ad
Anton Petrosyan <aspetrosyan@avito.ru>
2022-01-17 11:59:08 +0300

    add save to file

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/ixpectus/go-curl-proxy
 
 go 1.17
 
-require github.com/elazarl/goproxy v0.0.0-20210801061803-8e322dfb79c4 // indirect
+require (
+	github.com/elazarl/goproxy v0.0.0-20210801061803-8e322dfb79c4 // indirect
+	github.com/ixpectus/fw v0.0.0-20190926092407-d1177f32c47b // indirect
+	github.com/mattn/go-colorable v0.1.2 // indirect
+	github.com/mattn/go-isatty v0.0.8 // indirect
+	github.com/moul/http2curl v1.0.0 // indirect
+	github.com/wtertius/pp v3.1.0+incompatible // indirect
+	golang.org/x/sys v0.0.0-20190222072716-a9d3bda3a223 // indirect
+)
--- proxy.go
+++ proxy.go
@@ -1,26 +1,41 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
 	"github.com/elazarl/goproxy"
+	"github.com/ixpectus/fw"
+	"github.com/moul/http2curl"
+)
+
+var (
+	logDirectory = flag.String("d", "./queries", "directory for logs")
+	verbose      = flag.Bool("v", false, "should every proxy request be logged to stdout")
+	addr         = flag.String("addr", ":9998", "proxy listen address")
 )
 
 func main() {
-	verbose := flag.Bool("v", false, "should every proxy request be logged to stdout")
-	addr := flag.String("addr", ":9999", "proxy listen address")
 	flag.Parse()
+	fmt.Printf("\n>>> startint at port %v <<< debug\n", *addr)
+
 	proxy := goproxy.NewProxyHttpServer()
 	proxy.OnRequest().HandleConnect(goproxy.AlwaysMitm)
 	proxy.OnRequest().DoFunc(func(req *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
-		fmt.Printf("\n>>> %v <<< debug\n", req)
-		// if req.URL.Scheme == "https" {
-		// 	req.URL.Scheme = "http"
-		// }
+		command, _ := http2curl.GetCurlCommand(req)
+		fw.WriteNewFileByMask(*logDirectory, ctx.Req.URL.Hostname()+"-req", []byte(command.String()))
 		return req, nil
+	})
+	proxy.OnResponse().DoFunc(func(resp *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
+		bodyBytes, _ := ioutil.ReadAll(resp.Body)
+		fw.WriteNewFileByMask(*logDirectory, ctx.Req.URL.Hostname()+"-resp", bodyBytes)
+		resp.Body.Close() //  must close
+		resp.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
+		return resp
 	})
 	proxy.Verbose = *verbose
 	log.Fatal(http.ListenAndServe(*addr, proxy))
